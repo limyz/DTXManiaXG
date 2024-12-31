89d64fd3f04d8542b0624915232afab4f98eec23
BocuD <duco.born@gmail.com>
2024-12-31 11:58:31 +0100

    Use Unicode for log output
    Log files, unlike config files, aren't something we need to read old versions from. Therefore, moving over to Unicode doesn't come with any drawbacks for these.

--- DTXMania/Code/App/CDTXMania.cs
+++ DTXMania/Code/App/CDTXMania.cs
@@ -1966,7 +1966,7 @@ for (int i = 0; i < 3; i++) {
             {
                 try
                 {
-                    Trace.Listeners.Add(new CTraceLogListener(new StreamWriter("DTXManiaLog.txt", false, Encoding.GetEncoding("shift-jis"))));
+                    Trace.Listeners.Add(new CTraceLogListener(new StreamWriter("DTXManiaLog.txt", false, Encoding.Unicode)));
                 }
                 catch (System.UnauthorizedAccessException)			// #24481 2011.2.20 yyagi
                 {
