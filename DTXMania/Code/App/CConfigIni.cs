67841257c0520aa7038610ad89e07d46364e0236
BocuD <duco.born@gmail.com>
2025-01-13 11:30:01 +0100

    Remove remaining reference to CDTXVersion

--- DTXMania/Code/App/CConfigIni.cs
+++ DTXMania/Code/App/CConfigIni.cs
@@ -2560,7 +2560,6 @@ namespace DTXMania
 				StreamReader reader = new StreamReader( this.ConfigIniファイル名, Encoding.GetEncoding( "Shift_JIS" ) );
 				str = reader.ReadToEnd();
 				tReadFromString( str );
-				CDTXVersion version = new CDTXVersion( this.strDTXManiaのバージョン );
 			}
 		}
 
